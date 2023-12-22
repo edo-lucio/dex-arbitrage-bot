2f29ccaef6fedac5ee74c9000f63429850c58e58
edo-lucio <assassineeeto@gmail.com>
2023-12-22 22:19:45 +0100

    config files

--- src/get_data/getPriceImpact.js
+++ src/get_data/getPriceImpact.js
@@ -53,17 +53,25 @@ async function main() {
         limit: 1000,
         lower_bound: 0,
     });
+
     const pools = [];
 
     // We have to get all pools with fetched ticks for them
     for (const p of rows) {
-        console.log(p.tokenA, p.tokenB);
         const {
             id,
             tokenA,
             tokenB,
             currSlot: { sqrtPriceX64, tick },
         } = p;
+
+        console.log("Good News");
+
+        const ticks = await getAllRows({
+            scope: id,
+            table: "ticks",
+            code: "swap.alcor",
+        });
 
         pools.push(
             new Pool({
@@ -72,6 +80,7 @@ async function main() {
                 tokenB: parseToken(tokenB),
                 sqrtPriceX64,
                 tickCurrent: tick,
+                ticks: ticks.sort((a, b) => a.id - b.id),
             })
         );
     }
@@ -79,19 +88,26 @@ async function main() {
     // 1.0000 EOS
     const amountIn = CurrencyAmount.fromRawAmount(
         new Token("eosio.token", 8, "WAX"),
-        1_0000
+        1_00000000
     );
     const tokenOut = new Token("alien.worlds", 4, "TLM");
     const slippage = new Percent(3, 100); // 0.3%
-    const receiver = "myaccount";
+    const receiver = "marcantonio4";
 
     // First trade sorted by biggest output
     const [trade] = await Trade.bestTradeExactIn(pools, amountIn, tokenOut, {
-        maxHops: 1,
+        maxHops: 3,
     });
+
+    const route = trade.swaps.pools.map((p) => p.id);
 
     const maxSent = trade.inputAmount;
     const minReceived = trade.minimumAmountOut(slippage);
+
+    // Memo Format <Service Name>#<Pool ID's>#<Recipient>#<Output Token>#<Deadline>
+    const memo = `swapexactin#${route.join(
+        ","
+    )}#${receiver}#${minReceived.toExtendedAsset()}#0`;
 
     const result = {
         input: trade.inputAmount.toFixed(),
@@ -99,6 +115,8 @@ async function main() {
         minReceived: minReceived.toFixed(),
         maxSent: maxSent.toFixed(),
         priceImpact: trade.priceImpact.toSignificant(2),
+        memo,
+        route,
         executionPrice: {
             numerator: trade.executionPrice.numerator.toString(),
             denominator: trade.executionPrice.denominator.toString(),
